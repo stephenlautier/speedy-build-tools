b79098aab9da179a70c78a3696aeb3fbf1586d7a
Christopher Magri <christopher.magri@betssongroup.com>
2016-06-17 09:45:07 +0200

    fixed according to CR

--- build/tasks/hook.js
+++ build/tasks/hook.js
@@ -1,10 +1,10 @@
 var config = require("../config");
-
 var gulp = require("gulp");
 var $ = require("gulp-load-plugins")(config.loadPluginsOptions);
 
 gulp.task("pre-commit", () => {
-	return $.gitGuppy(gulp).stream("pre-commit")
+	return $.gitGuppy(gulp)
+		.stream("pre-commit")
 		.pipe($.filter(config.src.ts))
 		.pipe($.tslint())
 		.pipe($.tslint.report($.tslintStylish, {
