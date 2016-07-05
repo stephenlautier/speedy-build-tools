e2e39d0b009068051f3044f1a0a04772075bab29
Clayton Lautier <clayton.lautier@betssongroup.com>
2016-07-05 13:55:39 +0200

    chore(package) Upgraded to Angular 2 RC3 style(lint) fixed some spaces

--- src/skeleton/skeleton.service.ts
+++ src/skeleton/skeleton.service.ts
@@ -5,9 +5,9 @@ export class SkeletonService {
 
 	log(logType: string, message: string, data?: any): void {
 		if (data) {
-			(<any>console)[logType](message, data);
+			(<any> console)[logType](message, data);
 		} else {
-			(<any>console)[logType](message);
+			(<any> console)[logType](message);
 		}
 	}
 
