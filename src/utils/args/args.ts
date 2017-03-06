25040b56fa9aee7d5078762bb2d10bf23ef9905f
Alan Agius <alan.agius@betssongroup.com>
2017-03-06 11:02:23 +0100

    remove redundent types

--- src/utils/args/args.ts
+++ src/utils/args/args.ts
@@ -15,7 +15,7 @@ export namespace Args {
 	setBoolean("debug", false);
 
 	export function setArray<T>(key: string, values: T[], defaultValue?: T, alias?: string) {
-		set<T>(key, defaultValue, alias);
+		set(key, defaultValue, alias);
 
 		if (values) {
 			yargs.choices(key, values);
@@ -25,12 +25,12 @@ export namespace Args {
 	}
 
 	export function setBoolean(key: string, defaultValue?: boolean, alias?: string) {
-		set<boolean>(key, defaultValue, alias);
+		set(key, defaultValue, alias);
 		yargs.boolean(key);
 	}
 
 	export function setNumber(key: string, defaultValue?: number, alias?: string) {
-		set<number>(key, defaultValue, alias);
+		set(key, defaultValue, alias);
 		yargs.number(key);
 	}
 
