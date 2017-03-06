56555b051c05c5a3d512713882a75859f31deed2
Alan Agius <alan.agius@betssongroup.com>
2017-03-06 11:13:54 +0100

    remove redundant types

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -27,7 +27,7 @@ export class Logger {
 		console.warn(yellow(this.formatMessage(message)));
 	}
 
-	error(message: string | null, error?: Error) {
+	error(message?: string, error?: Error) {
 		console.error(red(this.formatMessage(this.formatErrorMessage(message, error))));
 	}
 
@@ -38,7 +38,7 @@ export class Logger {
 		return `${white(`[${time}]`)} ${cyan(`${this.scope}:`)} ${message}`;
 	}
 
-	private formatErrorMessage(message: string | null, error?: Error): string {
+	private formatErrorMessage(message?: string, error?: Error): string {
 		if (error) {
 			const errorMsg = error.message ? error.message : JSON.stringify(error);
 			return `Error: ${message ? `${message}, ${errorMsg}` : errorMsg}`;
